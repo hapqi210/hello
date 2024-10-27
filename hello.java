cd8b5b745d3deb6e7150362a0e96e2fa42dc5468
hapqi210 <157613652+hapqi210@users.noreply.github.com>
2024-10-27 23:37:28 +0700

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class IntoduceYourself  {
+  public static void main(String[]args)  {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("              HELLO         ");
+    String name = "  Lý Tấn Lộc  ";
+    String hobby = "Chơi game";
+    System.out.println("  Chào! Tao tên là" + name);
+    System.out.println("               🐧           ");
+    }
+ }
